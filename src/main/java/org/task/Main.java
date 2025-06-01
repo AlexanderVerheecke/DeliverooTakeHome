bdbf6c44083d06b0c8a7e66fb3e59592e90eff27
AlexanderVerheecke <alexander.verheecke@hotmail.com>
2025-06-01 11:18:50 +0100

    fixed main, use arg for exectuable

--- src/main/java/org/task/Main.java
+++ src/main/java/org/task/Main.java
@@ -7,7 +7,7 @@ public class Main {
             return;
         }
         try {
-            Cron cron =  new Cron("*/15 0 1,15 * 1-5 /usr/bin/find");
+            Cron cron =  new Cron(args[0]);
             cron.printExression();
         } catch (IllegalArgumentException e) {
             System.out.println("Caught expected exception: " + e.getMessage());
